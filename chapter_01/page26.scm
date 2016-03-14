c3f8737611bca2dc2eed83dfa2854e0a15302980
wangoasis <wangqingsky@outlook.com>
2016-03-14 16:37:50 +0800

    mofified page26.scm, added page20.scm

--- chapter_01/page26.scm
+++ chapter_01/page26.scm
@@ -11,7 +11,7 @@
   )
 
   ( define ( cc amount kind-of-coins ) 
-    ( cond ( ( = amount 1) 1 ) 
+    ( cond ( ( = amount 0) 1 ) 
            ( ( or ( < amount 0) ( = kind-of-coins 0) ) 0 )
            ( else ( + ( cc amount ( - kind-of-coins 1) )
                       ( cc ( - amount ( first-dimension kind-of-coins) )
